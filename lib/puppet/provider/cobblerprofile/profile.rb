243641537ffb121714936d037a9407f23c2f0726
Igor Galić <i.galic@brainsware.org>
2013-10-10 11:16:30 +0200

    typo: to_ts -> to_s

--- lib/puppet/provider/cobblerprofile/profile.rb
+++ lib/puppet/provider/cobblerprofile/profile.rb
@@ -33,7 +33,7 @@ Puppet::Type.type(:cobblerprofile).provide(:profile) do
         :virt_file_size   => member['virt_file_size'].to_s,
         :virt_cpus        => member['virt_cpus'].to_s,
         :virt_path        => member['virt_path'],
-        :virt_ram         => member['virt_ram'].to_ts,
+        :virt_ram         => member['virt_ram'].to_s,
         :virt_type        => member['virt_type'],
       )
     end
